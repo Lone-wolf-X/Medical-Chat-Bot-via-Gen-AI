a3064820113e98220ae5c0d2275208539617f979
Anurag Singh <106446001+Lone-wolf-X@users.noreply.github.com>
2025-03-10 02:37:10 +0530

    floder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Project',
+    version= '0.0.0',
+    author = ' Anurag',
+    author_email ='6678130@gmai.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
